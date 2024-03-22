a81b9c3c658fb9f75a2f00954656376872647bb7
brani <branislannjemec@gmail.com>
2024-03-23 00:56:55 +0100

    Create db configuration for ApplicationUser

---
+++ RMS.Infrastructure/Domain/ApplicationUserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RMS.Domain.Entities;
+
+namespace RMS.Infrastructure.Domain;
+
+public class ApplicationUserConfiguration :IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasMany(x => x.Roles).WithOne().HasForeignKey(x => x.Id).IsRequired().OnDelete(DeleteBehavior.Cascade);
+        
+    }
+}
