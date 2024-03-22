032087053c83cd42f31fbd6434411aebec4fe4b1
brani <branislannjemec@gmail.com>
2024-03-23 00:56:35 +0100

    create applicationUser entity

---
+++ RMS.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RMS.Domain.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+    
+    public string? Email { get; set; }
+    
+    public string? Password { get; set; }
+    
+    public IList<IdentityRole> Roles { get; } = new List<IdentityRole>();
+    
+    
+}
