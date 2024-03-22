2432d85d01ffda20a6603b62595952739aab0fef
brani <branislannjemec@gmail.com>
2024-03-23 00:57:47 +0100

    install some dependencies

--- RMS.Application/obj/Debug/net8.0/RMS.Application.AssemblyInfo.cs
+++ RMS.Application/obj/Debug/net8.0/RMS.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMS.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e38ff024cd8af2f31eee4129e89635a707734524")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMS.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMS.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
