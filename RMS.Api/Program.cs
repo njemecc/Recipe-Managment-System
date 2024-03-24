afc5844ebd93d6fc890437b8440d18221ae48bce
brani <branislannjemec@gmail.com>
2024-03-24 12:43:55 +0100

    addInfrastructure to the Program.cs

--- RMS.Api/Program.cs
+++ RMS.Api/Program.cs
@@ -1,10 +1,12 @@
+using RMS.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
