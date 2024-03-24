e20064ab52d16d91c2dae59b88a7fceca358af19
brani <branislannjemec@gmail.com>
2024-03-24 12:44:32 +0100

    create recipe entity

---
+++ RMS.Domain/Entities/Recipe.cs
@@ -0,0 +1,27 @@
+﻿namespace RMS.Domain.Entities;
+
+public class Recipe
+{
+    public Guid Id
+    {
+        get;
+        private set;
+    }
+    
+    public string Title { get; set; }
+    
+    public string Instruction { get; set; }
+    
+    public Category Category { get; set; }
+    
+    public IList<RecipeIngrediant> RecipeIngrediants { get; set; } 
+    
+    public ApplicationUser User
+    {
+        get;
+        private set;
+    }
+    
+    
+    
+}
