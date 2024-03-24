9f4d5e8d829772626349689ee7b5556ae099064d
brani <branislannjemec@gmail.com>
2024-03-24 12:41:06 +0100

    create category entity

---
+++ RMS.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace RMS.Domain.Entities;
+
+public class Category
+{
+    
+    public Guid Id
+    {
+        get;
+        private set;
+    }
+    
+    public string Name { get; set; }
+    
+    public IList<Recipe> Recipes { get; set; } = new List<Recipe>();
+}
