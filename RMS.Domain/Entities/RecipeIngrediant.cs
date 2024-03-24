e98bf3aa647b6afe49c181d41df0ad8ded8bd7c5
brani <branislannjemec@gmail.com>
2024-03-24 12:45:06 +0100

    Create recipeIngrediant intersection table entity

---
+++ RMS.Domain/Entities/RecipeIngrediant.cs
@@ -0,0 +1,10 @@
+﻿namespace RMS.Domain.Entities;
+
+public class RecipeIngrediant
+{
+    public Guid RecipeId { get; set; }
+    public Recipe Recipe { get; set; }
+
+    public Guid IngredientId { get; set; }
+    public Ingredient Ingredient { get; set; }
+}
