4d6139c9749afe9aceceba9ebb6a053e21a705df
brani <branislannjemec@gmail.com>
2024-03-24 12:45:38 +0100

    create a configuration for recipe and recipeIngredient

---
+++ RMS.Infrastructure/Domain/RecipeIngredient/RecipeIngredientConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RMS.Domain.Entities;
+
+namespace RMS.Infrastructure.Domain.RecipeIngredient;
+
+public class RecipeIngredientConfiguration : IEntityTypeConfiguration<RecipeIngrediant>
+{
+    public void Configure(EntityTypeBuilder<RecipeIngrediant> builder)
+    {
+        builder.HasKey(ri => new { ri.RecipeId, ri.IngredientId });
+
+        builder.HasOne(ri => ri.Recipe).WithMany(r => r.RecipeIngrediants).HasForeignKey(ri => ri.RecipeId);
+        
+        builder.HasOne(ri => ri.Ingredient)
+            .WithMany(i => i.RecipeIngredients)
+            .HasForeignKey(ri => ri.IngredientId);
+    }
+}
