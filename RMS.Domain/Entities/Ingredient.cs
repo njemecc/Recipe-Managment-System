fbe711723339a3eb286c62eabc6f2eba68e82cc5
brani <branislannjemec@gmail.com>
2024-03-24 12:41:22 +0100

    Create ingredient entity

---
+++ RMS.Domain/Entities/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿namespace RMS.Domain.Entities;
+
+public class Ingredient
+{
+    public Guid Id
+    {
+        get;
+        private set;
+    }
+    public string Name { get; set; }
+    
+    public IList<RecipeIngrediant> RecipeIngredients { get; set; }
+}
