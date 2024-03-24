28c4d7e4e857f1bb0f55074ba8a4a8920de69eb8
brani <branislannjemec@gmail.com>
2024-03-24 12:40:37 +0100

    add Id property

--- RMS.Domain/Entities/ApplicationUser.cs
+++ RMS.Domain/Entities/ApplicationUser.cs
@@ -4,6 +4,13 @@ namespace RMS.Domain.Entities;
 
 public class ApplicationUser : IdentityUser
 {
+    
+    public Guid Id
+    {
+        get;
+        private set;
+    }
+    
     public string? FirstName { get; set; }
     
     public string? LastName { get; set; }
@@ -13,6 +20,8 @@ public class ApplicationUser : IdentityUser
     public string? Password { get; set; }
     
     public IList<IdentityRole> Roles { get; } = new List<IdentityRole>();
-    
-    
+
+    public IList<Recipe> Recipes { get; set; } = new List<Recipe>();
+
+
 }
