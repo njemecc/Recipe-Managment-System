8310b93b64e5f9860dbd25ac6977198e0e2178c7
brani <branislannjemec@gmail.com>
2024-03-26 18:27:23 +0100

    create CategoryCreateCommand and Handler

---
+++ RMS.Application/Categories/commands/CategoryCreateCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using RMS.Application.Common.Dto.Category;
+
+namespace RMS.Application.Categories.commands;
+
+public record CategoryCreateCommand(CategoryCreateDto Category) : IRequest<CategoryDetailsDto>;
