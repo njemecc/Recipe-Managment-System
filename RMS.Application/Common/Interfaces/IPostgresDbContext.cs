b1b4152463e54e17111b24171b75baf6a076be60
brani <branislannjemec@gmail.com>
2024-03-26 17:08:53 +0100

    Create DbSets for Categories Ingredients Recipes and RecipeIngredients

--- RMS.Application/Common/Interfaces/IPostgresDbContext.cs
+++ RMS.Application/Common/Interfaces/IPostgresDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace RMS.Application.Common.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using RMS.Domain.Entities;
+
+namespace RMS.Application.Common.Interfaces;
 
 public interface IPostgresDbContext
 {
+    
+    public DbSet<ApplicationUser> Users { get; }
+    public DbSet<Recipe> Recipes { get; }
+    
+    public DbSet<Category> Categories { get; }
+    
+    public DbSet<Ingredient> Ingredients { get; }
+    
+    public DbSet<RecipeIngrediant> RecipeIngrediants { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
