ac122c877f15c937f1c0290dc3d72fbf21f9a1f0
brani <branislannjemec@gmail.com>
2024-03-26 17:49:12 +0100

    addApplication, addControlers and mapControllers

--- RMS.Api/Program.cs
+++ RMS.Api/Program.cs
@@ -1,12 +1,15 @@
+using RMS.Application;
 using RMS.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +40,9 @@ app.MapGet("/weatherforecast", () =>
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
+
+app.MapControllers();
 
 app.Run();
 
