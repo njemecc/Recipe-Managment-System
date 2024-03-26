4835c65e95a061b9a281c86952ec855921ecf62f
brani <branislannjemec@gmail.com>
2024-03-26 17:51:48 +0100

    change to better variable names

--- RMS.Infrastructure/Domain/RecipeIngredient/RecipeIngredientConfiguration.cs
+++ RMS.Infrastructure/Domain/RecipeIngredient/RecipeIngredientConfiguration.cs
@@ -10,10 +10,10 @@ public class RecipeIngredientConfiguration : IEntityTypeConfiguration<RecipeIngr
     {
         builder.HasKey(ri => new { ri.RecipeId, ri.IngredientId });
 
-        builder.HasOne(ri => ri.Recipe).WithMany(r => r.RecipeIngrediants).HasForeignKey(ri => ri.RecipeId);
+        builder.HasOne(ri => ri.Recipe).WithMany(r => r.Ingrediants).HasForeignKey(ri => ri.RecipeId);
         
         builder.HasOne(ri => ri.Ingredient)
-            .WithMany(i => i.RecipeIngredients)
+            .WithMany(i => i.Receipts)
             .HasForeignKey(ri => ri.IngredientId);
     }
 }
