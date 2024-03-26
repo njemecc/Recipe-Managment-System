68b323459f3b30f1dde0d7a77b9c30409f3c4649
brani <branislannjemec@gmail.com>
2024-03-26 17:09:23 +0100

    add DbPort

--- RMS.Infrastructure/Configuration/PostgresDbConfiguration.cs
+++ RMS.Infrastructure/Configuration/PostgresDbConfiguration.cs
@@ -6,6 +6,8 @@ public class PostgresDbConfiguration
     
     public string? DbName { get; set; }
     
+    public string? DbPort { get; set; }
+    
     public string? UserName { get; set; }
     
     public string? Password { get; set; }
