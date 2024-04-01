003a28ea6d8697e77a4783c015a6fad1b6a62a2c
brani <branislannjemec@gmail.com>
2024-04-01 18:53:09 +0200

    create SdkClient interface and implementation

---
+++ RMS.SDK/RMS.SDK.Application/Client/RMSSdkClient.cs
@@ -0,0 +1,14 @@
+﻿using RMS.SDK.Application.Extensions;
+using RMS.SDK.Application.Models;
+
+namespace RMS.SDK.Application.Client;
+
+public class RmsSdkClient(IRMSApi api) : IRmsSdkClient
+{
+    public async Task<RmsCategoryCreateResponseModel> CreateCategoryAsync(RmsCategoryCreateRequestModel request)
+    {
+        var result =  await api.CreateCategoryAsync(request.ToDto());
+
+        return result.ToModel();
+    }
+}
