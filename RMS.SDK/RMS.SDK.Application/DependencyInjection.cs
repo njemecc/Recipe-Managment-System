ec37f1b017ae2528450bd6c704bc322eace1cd63
brani <branislannjemec@gmail.com>
2024-04-01 18:51:59 +0200

    create DependencyInjection

---
+++ RMS.SDK/RMS.SDK.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RMS.SDK.Application.Client;
+
+namespace RMS.SDK.Application;
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddRmsSdk(this IServiceCollection services)
+    {
+        services.AddScoped<IRmsSdkClient, RmsSdkClient>();
+        return services;
+    }
+    
+}
