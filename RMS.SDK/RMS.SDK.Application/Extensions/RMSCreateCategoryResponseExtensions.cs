5a7be3d241b2bb349f6277e9c616f94b7b458325
brani <branislannjemec@gmail.com>
2024-04-01 18:53:45 +0200

    create RMSCreateCategoryResponseExtensions

---
+++ RMS.SDK/RMS.SDK.Application/Extensions/RMSCreateCategoryResponseExtensions.cs
@@ -0,0 +1,12 @@
+﻿using RMS.SDK.Application.Models;
+using RMS.SDK.Dto;
+
+namespace RMS.SDK.Application.Extensions;
+
+public static class RmsCreateCategoryResponseExtensions
+{
+    public static RmsCategoryCreateResponseModel ToModel(this RmsCategoryCreateResponseDto dto)
+    {
+        return new RmsCategoryCreateResponseModel(dto.Name);
+    }
+}
