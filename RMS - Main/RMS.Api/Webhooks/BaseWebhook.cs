9848419c0074278c39f138811a44616dfdadaf2f
brani <branislannjemec@gmail.com>
2024-04-09 13:20:59 +0200

    create BaseWebhook

---
+++ RMS - Main/RMS.Api/Webhooks/BaseWebhook.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RMS.Api.Webhooks;
+
+[ApiController]
+[Route("webhook/[controller]/[action]")]
+public class BaseWebhook : ControllerBase
+{
+    private ISender? _mediator;
+
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+
+}
