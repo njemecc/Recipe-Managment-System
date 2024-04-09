ea8be3d3649422dd0244ad0261779ead907f699a
brani <branislannjemec@gmail.com>
2024-04-09 13:21:25 +0200

    create CategoryCreationWebHook

---
+++ RMS - Main/RMS.Api/Webhooks/CategoryCreationWebHook.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RMS.Api.Auth.Constants;
+using RMS.Application.Categories.commands;
+
+namespace RMS.Api.Webhooks;
+
+
+[Authorize(AuthenticationSchemes = nameof(AuthConstants.HeaderBasicAuthenticationScheme))]
+public class CategoryCreationWebHook : BaseWebhook
+{
+    public async Task<IActionResult> Create(CategoryCreateCommand command) => Ok(await Mediator.Send(command));
+}
