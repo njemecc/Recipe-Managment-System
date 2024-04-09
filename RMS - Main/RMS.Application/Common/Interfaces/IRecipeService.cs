5956331082005a8b13cee1e12121aabfa282f126
brani <branislannjemec@gmail.com>
2024-04-09 13:21:45 +0200

    create RecipeService

---
+++ RMS - Main/RMS.Application/Common/Interfaces/IRecipeService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using RMS.Application.Common.Dto.Recipe;
+
+namespace RMS.Application.Common.Interfaces;
+
+public interface IRecipeService
+{
+  Task<RecipeDetailsDto> Created(RecipeCreateDto recipe);
+}
---
+++ RMS - Main/RMS.Infrastructure/services/RecipeService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMS.Application.Common.Dto.Recipe;
+using RMS.Application.Common.Interfaces;
+using RMS.Application.Common.Mappers;
+using RMS.Application.Recipes.commands;
+using RMS.Domain.Entities;
+
+namespace RMS.Infrastructure.services;
+
+public class RecipeService(IPostgresDbContext dbContext) : IRecipeService
+{
+    public async Task<RecipeDetailsDto> Created(RecipeCreateDto recipe)
+    {
+        var user = await dbContext.Users.Where(x => x.Id == recipe.UserId).FirstOrDefaultAsync();
+
+        var category = await dbContext.Categories.Where(x => x.Id == recipe.CategoryId)
+            .FirstOrDefaultAsync();
+        
+        if (user == null || category == null )
+        {
+            throw new Exception("User or Category with this ID does not exists");
+        }
+        
+        var recipeEntity = recipe.FromCreateDtoToEntity().AddUser(user).AddCategory(category);
+
+        foreach (var ing in recipe.Ingredients)
+        {
+            var ingrediantEntity = ing.FromCreateIngredientDtoToEntity();
+               
+            dbContext.Ingredients.Add(ing.FromCreateIngredientDtoToEntity());
+
+            var recipeIngredient = new RecipeIngrediant
+            {
+                Recipe = recipeEntity,
+                Ingredient = ingrediantEntity
+            };
+
+            dbContext.RecipeIngrediants.Add(recipeIngredient);
+
+        }
+        
+        var result = dbContext.Recipes.Add(recipeEntity);
+
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        //pokusati i ToDto(); ovde da vidimo da li automatski mapira 
+        return recipeEntity.ToCustomDetailsDto();
+    }
+}
