91eab2d48e608bb8c0d49438ea53cf71efda0780
brani <branislannjemec@gmail.com>
2024-04-11 00:01:27 +0200

    create UserBuilder

---
+++ RMS - Main/RMS.BaseTests/Builders/Domain/UserBuilder.cs
@@ -0,0 +1,41 @@
+﻿using RMS.Domain.Entities;
+
+namespace RMS.BaseTests.Builders.Domain
+{
+    public class UserBuilder
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _password;
+
+        public UserBuilder WithFirstName(string firstName)
+        {
+            _firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder WithLastName(string lastName)
+        {
+            _lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder WithEmail(string email)
+        {
+            _email = email;
+            return this;
+        }
+
+        public UserBuilder WithPassword(string password)
+        {
+            _password = password;
+            return this;
+        }
+
+        public ApplicationUser Build()
+        {
+            return new ApplicationUser(_firstName, _lastName, _email, _password);
+        }
+    }
+}
