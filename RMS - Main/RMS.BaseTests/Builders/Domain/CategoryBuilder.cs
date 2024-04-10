55405a8d0e27c4d8eeb65c082bbcdf63d566fde2
brani <branislannjemec@gmail.com>
2024-04-10 23:59:57 +0200

    create CategoryBuilder.cs

---
+++ RMS - Main/RMS.BaseTests/Builders/Domain/CategoryBuilder.cs
@@ -0,0 +1,20 @@
+﻿using RMS.Domain.Entities;
+
+namespace RMS.BaseTests.Builders.Domain
+{
+    public class CategoryBuilder
+    {
+        private string _name;
+
+        public CategoryBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public Category Build()
+        {
+            return new Category(_name);
+        }
+    }
+}
