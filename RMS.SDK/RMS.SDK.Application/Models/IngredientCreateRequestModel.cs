4cbb3fd92a4f0d760802f41020724c28176b41c7
brani <branislannjemec@gmail.com>
2024-04-22 10:15:50 +0200

    map toDto

---
+++ RMS.SDK/RMS.SDK.Application/Models/IngredientCreateRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using RMS.SDK.Api.Dto.Ingredient;
+
+namespace RMS.SDK.Models;
+
+public class IngredientCreateRequestModel(string Name)
+{
+    public IngredientCreateRequestDto ToDto()
+    {
+        return new IngredientCreateRequestDto(new IngredientCreateDto(Name));
+    }
+}
