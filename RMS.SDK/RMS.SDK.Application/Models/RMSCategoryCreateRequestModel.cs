77530227360102eaee40cff47007a1547bd148f6
brani <branislannjemec@gmail.com>
2024-04-22 10:16:38 +0200

    create RMSCategoryCreateRequestModel

---
+++ RMS.SDK/RMS.SDK.Application/Models/RMSCategoryCreateRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using RMS.SDK.Dto;
+
+namespace RMS.SDK.Application.Models;
+
+public record RmsCategoryCreateRequestModel(string Name)
+{
+    public RMSCategoryCreateRequestDto ToDto()
+    {
+        return new RMSCategoryCreateRequestDto(new RMSCategoryCreateDto(Name));
+    }
+}
