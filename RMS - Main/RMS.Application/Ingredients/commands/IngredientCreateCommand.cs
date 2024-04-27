597dab180aef1bff05ad064b4b82a75fe61cd69f
njemecc <branislannjemec@gmail.com>
2024-04-27 14:01:13 +0200

    create IngredientCreateCommand

---
+++ RMS - Main/RMS.Application/Ingredients/commands/IngredientCreateCommand.cs
@@ -0,0 +1,6 @@
+using MediatR;
+using RMS.Application.Common.Dto.Ingredient;
+
+namespace RMS.Application.Ingredients.commands;
+
+public record IngredientCreateCommand(IngredientCreateDto Ingredient) : IRequest<IngredientDetailsDto>;
