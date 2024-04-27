fc85aac6a60cbc426e8397a10d7d192b6f898d52
njemecc <branislannjemec@gmail.com>
2024-04-27 13:56:10 +0200

    create Encrypt and Decrypt functions

---
+++ RMS - Main/RMS.Application/Common/Extensions/AesEncryptionExtensions.cs
@@ -0,0 +1,20 @@
+using System.Security.Cryptography;
+
+namespace RMS.Application.Common.Extensions;
+
+public static class AesEncryptionExtensions
+{
+    public static string AesEncrypt(this string plainText, string key)
+    {
+
+        return "";
+
+    }
+
+
+
+    public static string AesDecrypt(this string text, string key)
+    {
+        return "";
+    }
+}
