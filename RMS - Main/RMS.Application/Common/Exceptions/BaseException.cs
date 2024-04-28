0b9b0fd2e4e0da933f6f51a15d04f964d5aa6e21
brani <branislannjemec@gmail.com>
2024-04-28 18:13:29 +0200

    create BaseException with additional Data

---
+++ RMS - Main/RMS.Application/Common/Exceptions/BaseException.cs
@@ -0,0 +1,13 @@
+﻿namespace RMS.Application.Common.Exceptions;
+
+
+public class BaseException : Exception
+{
+
+    public object? AdditionalData { get; }
+    
+    public  BaseException(string message, object? additionalData = null) : base(message)
+    {
+        AdditionalData = additionalData;
+    }
+}
