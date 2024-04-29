37b014176295932444b91d9672a546bd76cb514b
brani <branislannjemec@gmail.com>
2024-04-30 00:01:54 +0200

    create ApplicationUserManager

---
+++ RMS - Main/RMS.Infrastructure/Identity/ApplicationUserManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RMS.Domain.Entities;
+
+namespace RMS.Infrastructure.Identity;
+
+public class ApplicationUserManager(
+    IUserStore<ApplicationUser> store,
+    IOptions<IdentityOptions> optionsAccessor,
+    IPasswordHasher<ApplicationUser> passwordHasher,
+    IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+    IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+    ILookupNormalizer keyNormalizer,
+    IdentityErrorDescriber errors,
+    IServiceProvider services,
+    ILogger<UserManager<ApplicationUser>> logger) : UserManager<ApplicationUser>(store,
+    optionsAccessor,
+    passwordHasher,
+    userValidators,
+    passwordValidators,
+    keyNormalizer,
+    errors,
+    services,
+    logger)
+{
+}
