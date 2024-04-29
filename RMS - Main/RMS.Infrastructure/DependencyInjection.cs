e977df771ea9790f7cc841d5d98d32edd4cded02
brani <branislannjemec@gmail.com>
2024-04-30 00:04:01 +0200

    add services

--- RMS - Main/RMS.Infrastructure/DependencyInjection.cs
+++ RMS - Main/RMS.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,14 @@
 ﻿using System.Reflection;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RMS.Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
 using RMS.Application.Common.Interfaces;
+using RMS.Domain.Entities;
 using RMS.Infrastructure.Context;
+using RMS.Infrastructure.Identity;
 using RMS.Infrastructure.services;
 
 namespace RMS.Infrastructure;
@@ -25,13 +28,20 @@ public static class DependencyInjection
                 x => x.MigrationsAssembly(typeof(PostgresDbContext).Assembly.FullName)));    
         }
 
-       
+
+        services.AddIdentity<ApplicationUser, IdentityRole>().AddRoleManager<RoleManager<IdentityRole>>()
+            .AddUserManager<ApplicationUserManager>().AddEntityFrameworkStores<PostgresDbContext>()
+            .AddDefaultTokenProviders();
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddScoped<IPostgresDbContext>(provider => provider.GetRequiredService<PostgresDbContext>());
         services.AddScoped<IRecipeService, RecipeService>();
         services.AddScoped<IJwtProvider,JwtProvider>();
+        services.AddScoped<IAuthService,NsiAuthService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ICurrentUserService, CurrentUserService>();
+        services.Configure<NSI_JwtConfiguration>(configuration.GetSection("JwtConfiguration"));
         return services;
     }
     
