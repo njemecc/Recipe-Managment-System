47d3b08c31d8886a3febe09c16919da660732b40
brani <branislannjemec@gmail.com>
2024-04-30 00:04:30 +0200

    create UserService

---
+++ RMS - Main/RMS.Application/Common/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using RMS.Domain.Entities;
+
+namespace RMS.Application.Common.Interfaces;
+
+public interface IUserService
+{
+    Task CreateUserAsync(string emailAddress,string firstName, string lastName, string password, List<string> roles);
+    Task<ApplicationUser?> GetUserAsync(string id);
+    Task<ApplicationUser?> GetUserByEmailAsync(string id);
+    Task<bool> IsInRoleAsync(ApplicationUser user, string roleName);
+}
