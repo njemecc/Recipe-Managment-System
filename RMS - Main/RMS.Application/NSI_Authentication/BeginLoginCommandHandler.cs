2fd5caf9aeb963857be5c887bcb423a0ed4aaf6b
brani <branislannjemec@gmail.com>
2024-04-30 00:02:51 +0200

    Create BeginLoginCommand and Handler

---
+++ RMS - Main/RMS.Application/NSI_Authentication/BeginLoginCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using RMS.Application.Common.Dto.NSI_Authentication;
+using RMS.Application.Common.Interfaces;
+
+namespace RMS.Application.NSI_Authentication;
+
+public class BeginLoginCommandHandler
+{
+    public class BeginLoginHandler(IAuthService authService) : IRequestHandler<BeginLoginCommand, BeginLoginResponseDto>
+    {
+        public async Task<BeginLoginResponseDto> Handle(BeginLoginCommand request, CancellationToken cancellationToken) => await authService.BeginLoginAsync(request.EmailAddresses);
+    }
+}
