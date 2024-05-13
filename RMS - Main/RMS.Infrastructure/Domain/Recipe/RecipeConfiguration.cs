c25a00ac5d386bc7240a2b2904e76949801f0fb3
brani <branislannjemec@gmail.com>
2024-05-13 09:16:19 +0200

    dont create a categoryId field

--- RMS - Main/RMS.Infrastructure/Domain/Recipe/RecipeConfiguration.cs
+++ RMS - Main/RMS.Infrastructure/Domain/Recipe/RecipeConfiguration.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using RMS.Domain.Entities;
-using RMS.Domain.Enums;
 
 namespace RMS.Infrastructure.Domain.Recipe;
 
@@ -18,11 +16,6 @@ public class RecipeConfiguration : IEntityTypeConfiguration<RMS.Domain.Entities.
         builder.HasIndex("UserId");
         
         builder.HasOne(r => r.User).WithMany(u => u.Recipes).HasForeignKey("UserId").IsRequired();
-        builder.Property(r => r.Category)
-            .IsRequired().HasDefaultValue(Category.CookedMeal)
-            .HasConversion(
-                c => c.Value, 
-                v => Category.FromValue(v)); 
-
+        
     }
 }
