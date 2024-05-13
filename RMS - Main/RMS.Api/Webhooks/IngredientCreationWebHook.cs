9f748357b4c0845d2fbdfd704b3d73ded682a72d
brani <branislannjemec@gmail.com>
2024-05-13 23:09:53 +0200

    Create A webhook for creating an Ingredient

---
+++ RMS - Main/RMS.Api/Webhooks/IngredientCreationWebHook.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RMS.Api.Auth.Constants;
+using RMS.Application.Categories.commands;
+using RMS.Application.Ingredients.commands;
+
+namespace RMS.Api.Webhooks;
+
+
+[Authorize(AuthenticationSchemes = nameof(AuthConstants.HeaderBasicAuthenticationScheme))]
+public class IngredientCreationWebHook : BaseWebhook
+{
+    [HttpPost]
+    public async Task<IActionResult> Create(IngredientCreateCommand command) => Ok(await Mediator.Send(command));
+}
