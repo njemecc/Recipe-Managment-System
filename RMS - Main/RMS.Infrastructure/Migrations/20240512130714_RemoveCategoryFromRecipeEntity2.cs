33594ffa7e14559dc537bc32c75d78527d0bbac5
brani <branislannjemec@gmail.com>
2024-05-13 09:15:29 +0200

    create migrations for deleting a category from Recipe

---
+++ RMS - Main/RMS.Infrastructure/Migrations/20240512130714_RemoveCategoryFromRecipeEntity2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCategoryFromRecipeEntity2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Recipes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
