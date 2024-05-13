7cffcf390b4fa15aa897bfd36f235e93978cf775
brani <branislannjemec@gmail.com>
2024-05-13 09:17:18 +0200

    remove categoryName from RecipeDetailsDto

--- RMS - Main/RMS.Application/Common/Dto/Recipe/RecipeDetailsDto.cs
+++ RMS - Main/RMS.Application/Common/Dto/Recipe/RecipeDetailsDto.cs
@@ -4,4 +4,4 @@ namespace RMS.Application.Common.Dto.Recipe;
 
 
 //dodati kasnije i listu ingrediants
-public record RecipeDetailsDto(string Title, string Instruction, string UserName, string CategoryName);
+public record RecipeDetailsDto(string Title, string Instruction, string UserName);
