5287807a15b1a11acff211c260e2c9ea0ae10e16
njemecc <branislannjemec@gmail.com>
2024-05-14 12:18:56 +0200

    .

--- RMS - Main/RMS.Application/Ingredients/commands/IngredientCreateCommand.cs
+++ RMS - Main/RMS.Application/Ingredients/commands/IngredientCreateCommand.cs
@@ -4,3 +4,4 @@ using RMS.Application.Common.Dto.Ingredient;
 namespace RMS.Application.Ingredients.commands;
 
 public record IngredientCreateCommand(IngredientCreateDto Ingredient) : IRequest<IngredientDetailsDto>;
+
